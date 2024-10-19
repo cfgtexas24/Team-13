84a88aec4f32265931c3c1b1df3ff56d6c8c3fdc
Ami Shah <amishah121@utexas.edu>
2024-10-18 22:11:00 -0500

    added jobs page to employer dashboard

--- frontend/cfg13/src/components/dashboards/EmployerDash.jsx
+++ frontend/cfg13/src/components/dashboards/EmployerDash.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../navbar'
+import Jobs from '../jobs'
 
 const EmployerDash = () => {
   return (
     <div>
         <Navbar />
+        <Jobs/>
     </div>
   )
 }
