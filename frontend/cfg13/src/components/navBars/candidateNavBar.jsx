a324b0cf9e3ec60d620488d4f774e925d141af1c
AishwaryaSudarshan <aishwaryasudarshan18@gmail.com>
2024-10-19 05:14:54 -0500

    change routing of candidate dashboard and maps

--- frontend/cfg13/src/components/navBars/candidateNavBar.jsx
+++ frontend/cfg13/src/components/navBars/candidateNavBar.jsx
@@ -14,13 +14,13 @@ const Navbar = () => {
   const isActive = (path) => location.pathname === path;
 
   return (
-    <nav className="bg-gray-100">
+    <nav className="bg-white">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <div className="flex items-center">
             <button
               type="button"
-              className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-200 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              className="sm:hidden relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-200 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               onClick={toggleMobileMenu}
             >
               <span className="absolute -inset-0.5"></span>
@@ -35,13 +35,10 @@ const Navbar = () => {
                 </svg>
               )}
             </button>
-          </div>
-
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex flex-shrink-0 items-center">
+            <div className="flex items-center">
               <img className="h-8 w-auto" src={logo} alt="Logo" />
             </div>
-            <div className="hidden sm:ml-6 sm:block">
+            <div className="hidden sm:block sm:ml-6">
               <div className="flex space-x-4">
                 <Link
                   to="dashboard"
