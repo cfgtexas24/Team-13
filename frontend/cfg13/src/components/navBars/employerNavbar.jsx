71aea1c5f004f5ee3247ff5fa0455ff01284ba74
saanhub <balasaanvi@gmail.com>
2024-10-19 05:29:00 -0500

    Fixed Employer Navbar

--- frontend/cfg13/src/components/navBars/employerNavbar.jsx
+++ frontend/cfg13/src/components/navBars/employerNavbar.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../../assets/rebirthLogo.png';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/rebirthLogo.png";
 
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -22,31 +22,67 @@ const Navbar = () => {
               <span className="absolute -inset-0.5"></span>
               <span className="sr-only">Open main menu</span>
               {isMobileMenuOpen ? (
-                <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                <svg
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18 18 6M6 6l12 12"
+                  />
                 </svg>
               ) : (
-                <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                <svg
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
                 </svg>
               )}
             </button>
           </div>
-          
+
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex flex-shrink-0 items-center">
-              <img className="h-8 w-auto" src={logo}/>
+              <img className="h-8 w-auto" src={logo} />
             </div>
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
-                <Link to="/employer" className="rounded-md bg-yellow-400 px-3 py-2 text-sm font-medium text-white">Dashboard</Link>
-                <Link to="/employer/createJobPage" className="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-yellow-200 ">Create Job</Link>
-                <Link to="feedbackForm" className="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-yellow-200 ">Feedback</Link>
-                
+                <Link
+                  to="/employer"
+                  className="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-yellow-200"
+                >
+                  Dashboard
+                </Link>
+                <Link
+                  to="/employer/createJobPage"
+                  className="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-yellow-200 "
+                >
+                  Create Job
+                </Link>
+                <Link
+                  to="feedbackForm"
+                  className="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-yellow-200 "
+                >
+                  Feedback
+                </Link>
               </div>
             </div>
           </div>
-          
+
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <div className="relative ml-3">
               <button
@@ -65,8 +101,18 @@ const Navbar = () => {
 
               {isProfileMenuOpen && (
                 <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                  <Link to="profile" className="block px-4 py-2 text-sm text-gray-700">Your Profile</Link>
-                  <Link to="signout" className="block px-4 py-2 text-sm text-gray-700">Sign out</Link>
+                  <Link
+                    to="profile"
+                    className="block px-4 py-2 text-sm text-gray-700"
+                  >
+                    Your Profile
+                  </Link>
+                  <Link
+                    to="signout"
+                    className="block px-4 py-2 text-sm text-gray-700"
+                  >
+                    Sign out
+                  </Link>
                 </div>
               )}
             </div>
@@ -77,10 +123,30 @@ const Navbar = () => {
       {isMobileMenuOpen && (
         <div className="sm:hidden">
           <div className="space-y-1 px-2 pb-3 pt-2">
-            <Link to="/employer" className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white">Dashboard</Link>
-            <Link to="/team" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</Link>
-            <Link to="/projects" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</Link>
-            <Link to="/calendar" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</Link>
+            <Link
+              to="/employer"
+              className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
+            >
+              Dashboard
+            </Link>
+            <Link
+              to="/team"
+              className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+            >
+              Team
+            </Link>
+            <Link
+              to="/projects"
+              className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+            >
+              Projects
+            </Link>
+            <Link
+              to="/calendar"
+              className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+            >
+              Calendar
+            </Link>
           </div>
         </div>
       )}
