e429493af3f2696ef8043ff34a0e090af6b3e7ed
Tanmay Chhimwal <chhimwalt@gmail.com>
2024-10-18 19:35:07 -0500

    restructure nav bar route

--- frontend/cfg13/src/App.js
+++ frontend/cfg13/src/App.js
@@ -18,29 +18,16 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
     <div>
-      <h1>Basic Example</h1>
-
-      {/* Routes nest inside one another. Nested route paths build upon
-            parent route paths, and nested route elements render inside
-            parent route elements. See the note about <Outlet> below. */}
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="onboarding" element={<Onboarding />}>
-            {/* This route is a parent route for the nested EmployeeOnboarding
-                  and EmployerOnboarding routes. It's a good place to put
-                  layout elements like headers that should appear on all
-                  onboarding pages. */}
             <Route path="employee" element={<EmployeeOnboarding />} />
             <Route path="employer" element={<EmployerOnboarding />} />
           </Route>
           <Route index element={<Home />} />
           <Route path="profile" element={<Profile />} />
           <Route path="jobs" element={<Jobs />} />
-
-          {/* Using path="*"" means "match anything", so this route
-                acts like a catch-all for URLs that we don't have explicit
-                routes for. */}
-          <Route path="*" element={<NoMatch />} />
+          <Route path="*" element={<NoMatch />} /> 
         </Route>
       </Routes>
     </div>
--- frontend/cfg13/src/components/navbar.js
+++ frontend/cfg13/src/components/navbar.js
@@ -1,9 +1,31 @@
 import * as React from 'react';
 import Button from '@mui/material/Button'; 
+import { Link, Outlet } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-        <Button variant="text">Hi</Button>
+    <div>
+    {/* A "layout route" is a good place to put markup you want to
+        share across all the pages on your site, like navigation. */}
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+        <li>
+          <Link to="/jobs">Jobs</Link>
+        </li>
+        <li>
+          <Link to="/nothing-here">Nothing Here</Link>
+        </li>
+      </ul>
+    </nav>
+
+    <hr />
+  </div>
   );
 };
 
