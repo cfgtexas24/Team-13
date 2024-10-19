48eca6916b525ac93c4b4b204bb25de216fd4652
Tanmay Chhimwal <chhimwalt@gmail.com>
2024-10-18 19:16:46 -0500

    add onboarding routes

--- frontend/cfg13/src/App.js
+++ frontend/cfg13/src/App.js
@@ -1,8 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
+import Onboarding from "./components/Onboarding"
 
 import { Routes, Route, Outlet, Link } from "react-router-dom";
 import { ThemeProvider, createTheme } from "@mui/material";
+
+import EmployeeOnboarding from './forms/EmployeeOnboarding'
+import EmployerOnboarding from './forms/EmployerOnboarding'
 
 const theme = createTheme()
 export default function App() {
@@ -16,6 +20,14 @@ export default function App() {
             parent route elements. See the note about <Outlet> below. */}
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route path="onboarding" element={<Onboarding />}>
+            {/* This route is a parent route for the nested EmployeeOnboarding
+                  and EmployerOnboarding routes. It's a good place to put
+                  layout elements like headers that should appear on all
+                  onboarding pages. */}
+            <Route path="employee" element={<EmployeeOnboarding />} />
+            <Route path="employer" element={<EmployerOnboarding />} />
+          </Route>
           <Route index element={<Home />} />
           <Route path="profile" element={<Profile />} />
           <Route path="jobs" element={<Jobs />} />
---
+++ frontend/cfg13/src/forms/EmployerOnboarding.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EmployerOnboarding = () => {
+  return (
+    <div>EmployerOnboarding</div>
+  )
+}
+
+export default EmployerOnboarding
